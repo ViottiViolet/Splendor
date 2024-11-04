0c1fa8bad42d0194fd3150b334c27c5c1dec9732
ViottiViolet <viottiviolet@gmail.com>
2024-11-04 08:23:42 -0600

    Update SplendorGameScreen.java
    basic class initialization for game screen JFrame

--- src/Game/SplendorGameScreen.java
+++ src/Game/SplendorGameScreen.java
@@ -1,1 +1,26 @@
+package Game;
 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class SplendorGameScreen extends JFrame {
+
+  // Inner class to paint background image
+  class BackgroundPanel extends JPanel {
+      @Override
+      /*protected void paintComponent(Graphics g) {
+          super.paintComponent(g);
+          ImageIcon bg = new ImageIcon("src/Images/StartMenu/back.jpeg");
+          g.drawImage(bg.getImage(), 0, 0, getWidth(), getHeight(), this);
+      }*/
+  }
+  
+  public static void main(String[] args) {
+      //new SplendorGameScreen();
+  }
+
+{

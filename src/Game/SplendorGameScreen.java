767917a62233c3d948220814c6dee37c97b8be75
ViottiViolet <viottiviolet@gmail.com>
2024-11-04 19:07:47 -0600

    Update SplendorGameScreen.java

--- src/Game/SplendorGameScreen.java
+++ src/Game/SplendorGameScreen.java
@@ -12,11 +12,11 @@ public class SplendorGameScreen extends JFrame {
   // Inner class to paint background image
   class BackgroundPanel extends JPanel {
       @Override
-      /*protected void paintComponent(Graphics g) {
+      protected void paintComponent(Graphics g) {
           super.paintComponent(g);
-          ImageIcon bg = new ImageIcon("src/Images/StartMenu/back.jpeg");
+          ImageIcon bg = new ImageIcon("src/Images/GameMenu/GameBackground.png");
           g.drawImage(bg.getImage(), 0, 0, getWidth(), getHeight(), this);
-      }*/
+      }
   }
   
   public static void main(String[] args) {

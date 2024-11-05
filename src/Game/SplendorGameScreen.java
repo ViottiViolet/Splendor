6abc13bfe7e17774dbcd89019b59da633dcd5922
ViottiViolet <viottiviolet@gmail.com>
2024-11-04 19:13:13 -0600

    Update SplendorGameScreen.java

--- src/Game/SplendorGameScreen.java
+++ src/Game/SplendorGameScreen.java
@@ -9,6 +9,15 @@ import java.awt.event.MouseEvent;
 
 public class SplendorGameScreen extends JFrame {
 
+  public SplendorGameScreen() {
+    setTitle("Splendor Game");
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setMinimumSize(new Dimension(1200, 700));
+
+    BackgroundPanel panel = new BackgroundPanel();
+    panel.setLayout(null); // Custom layout
+  }
+  
   // Inner class to paint background image
   class BackgroundPanel extends JPanel {
       @Override

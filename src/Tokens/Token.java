418a518f4f96b15d620098102c58e443b2955645
Notiisx <103373313+Notiisx@users.noreply.github.com>
2024-11-17 19:05:19 -0600

    Updated Token class to be more readable, so I could implement it along with tokenLoader

--- src/Tokens/Token.java
+++ src/Tokens/Token.java
@@ -1,17 +1,37 @@
 package Tokens;
-import java.util.*;
-import java.io.*;
+
 public class Token {
-    private String tokenType, illustration;
+    private final TokenLoader.TokenType tokenType;
+    private final String illustration;
     private int numTokens;
-    public Token(String tokenType /*Player p*/, String numTokens){
+
+    public Token(TokenLoader.TokenType tokenType, int initialTokens) {
         this.tokenType = tokenType;
-        illustration = "src/TokenImages/token_" + tokenType + ".png";
+        this.numTokens = initialTokens;
+        this.illustration = "src/TokenImages/token_" + tokenType.getFilename() + ".png";
     }
 
-    public void increase(int amount){numTokens+=amount;}
-    public void decrease(int amount){if(numTokens > amount)numTokens-=amount;}
-    public String getTokenType() { return tokenType; }
-    public String getIllustration() { return illustration; }
-    public int getNumTokens() { return numTokens; }
+    public void increase(int amount) {
+        if (amount > 0) {
+            numTokens += amount;
+        }
+    }
+
+    public void decrease(int amount) {
+        if (amount > 0 && numTokens >= amount) {
+            numTokens -= amount;
+        }
+    }
+
+    public TokenLoader.TokenType getTokenType() {
+        return tokenType;
+    }
+
+    public String getIllustration() {
+        return illustration;
+    }
+
+    public int getNumTokens() {
+        return numTokens;
+    }
 }
